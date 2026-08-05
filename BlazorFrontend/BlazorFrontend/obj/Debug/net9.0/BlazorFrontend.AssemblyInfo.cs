80dc93da8896501e69922d9a37aa8a8a489d35f0
gezgale <najafzade@gmail.com>
2026-08-05 03:50:46 +0330

    Finished.

--- BlazorFrontend/BlazorFrontend/obj/Debug/net9.0/BlazorFrontend.AssemblyInfo.cs
+++ BlazorFrontend/BlazorFrontend/obj/Debug/net9.0/BlazorFrontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a36be06203e5cc0a8f6942959621c9089a793df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f845fa8dca6b6f937c46e5b701f39896c9a20b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
